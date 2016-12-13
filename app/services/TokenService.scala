fceb3861eab2a315b7ec724759e067f6ce33baaf
Rasmus Kuschel <rasmushofmann@gmail.com>
2016-12-13 13:58:03 +0100

    TokenService encode implemented

--- app/services/TokenService.scala
+++ app/services/TokenService.scala
@@ -1,11 +1,32 @@
 package services
 
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
 import play.api.libs.json.JsObject
 
 class TokenService(secret: String) {
 
-  def encode(header: JsObject, payload: JsObject): String =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
+  val charset = StandardCharsets.UTF_8
+  val algorithm = "HmacSHA256" // TODO read from header
+  val mac: Mac = Mac.getInstance(algorithm)
+  val secretKey: SecretKeySpec = new SecretKeySpec(secret.getBytes, algorithm)
+
+  mac.init(secretKey)
+
+  private def base64(in: Array[Byte]): String = Base64.getEncoder.encodeToString(in).replaceAll("=", "")
+
+  def encode(header: JsObject, payload: JsObject): String = {
+
+    val h = base64(header.toString.getBytes(charset))
+    val p = base64(payload.toString.getBytes(charset))
+
+    val s = base64(mac.doFinal(s"$h.$p".getBytes(StandardCharsets.UTF_8)))
+
+    s"$h.$p.$s"
+  }
 
 
   def decode(encoded: String): (JsObject, JsObject) = ???
